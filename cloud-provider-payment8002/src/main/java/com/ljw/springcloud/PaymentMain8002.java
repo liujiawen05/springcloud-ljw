a36a5758f139792e4d75819f8fc2f2ca9142d3c5
liujiawen0327 <liujiawen1667@163.com>
2020-04-15 03:48:39 +0800

    测试提交

---
+++ cloud-provider-payment8002/src/main/java/com/ljw/springcloud/PaymentMain8002.java
@@ -0,0 +1,20 @@
+package com.ljw.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Description:
+ * @Author: Ljw
+ * @Date: 2020/4/8.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
+public class PaymentMain8002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8002.class);
+    }
+}
