2016b5c748d6626c628d713a89561eeb5d3cccb1
liujiawen0327 <liujiawen1667@163.com>
2020-04-15 03:49:49 +0800

    测试提交

---
+++ cloud-provider-payment8004/src/main/java/com/ljw/springcloud/PaymentMain8004.java
@@ -0,0 +1,18 @@
+package com.ljw.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Description:
+ * @Author: Ljw
+ * @Date: 2020/4/9.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class);
+    }
+}
