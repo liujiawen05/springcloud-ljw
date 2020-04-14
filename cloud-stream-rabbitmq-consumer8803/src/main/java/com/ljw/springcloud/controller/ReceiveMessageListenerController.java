1073c3f8bbc7bbfcddee2ac97b70522c2539261f
liujiawen0327 <liujiawen1667@163.com>
2020-04-15 03:53:00 +0800

    测试提交

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/ljw/springcloud/controller/ReceiveMessageListenerController.java
@@ -0,0 +1,27 @@
+package com.ljw.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @Author: Ljw
+ * @Date: 2020/4/14.
+ */
+@RestController
+@EnableBinding(Sink.class)
+public class ReceiveMessageListenerController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message){
+        System.out.println("消费者2号，0------>接收到消息："+message.getPayload()+"\t port:"+serverPort);
+    }
+
+}
