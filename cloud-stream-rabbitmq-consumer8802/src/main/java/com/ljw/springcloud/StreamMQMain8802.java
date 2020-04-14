8d6c0af55e48e7611f45bed1ea1adbb85fded034
liujiawen0327 <liujiawen1667@163.com>
2020-04-15 03:50:35 +0800

    测试提交

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/ljw/springcloud/StreamMQMain8802.java
@@ -0,0 +1,16 @@
+package com.ljw.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author: Ljw
+ * @Date: 2020/4/14.
+ */
+@SpringBootApplication
+public class StreamMQMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8802.class);
+    }
+}
