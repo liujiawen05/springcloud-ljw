93190b747d477dab0f5d5120bb98574b3ce12152
liujiawen0327 <liujiawen1667@163.com>
2020-04-15 03:42:47 +0800

    11111

---
+++ cloud-config-center-3366/src/main/java/com/ljw/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,16 @@
+package com.ljw.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author: Ljw
+ * @Date: 2020/4/15.
+ */
+@SpringBootApplication
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class);
+    }
+}
